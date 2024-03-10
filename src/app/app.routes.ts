4217473d380b0f24440a8e5b642f1e0dd12b71d3
jgamonal <josue.gamonal@gmail.com>
2024-03-10 21:55:23 +0100

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewuserComponent } from './pages/newuser/newuser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", pathMatch: "full", redirectTo: "home" },
+	{ path: 'home', component: HomeComponent},
+	{path: 'newuser', component: NewuserComponent}
+];

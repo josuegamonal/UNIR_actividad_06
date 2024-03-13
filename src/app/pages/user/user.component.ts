90332cff0236553db4636dd7b90adfaf1328798a
jgamonal <josue.gamonal@gmail.com>
2024-03-13 19:23:38 +0100

    Petición http creada en servicio

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { IUser } from '../../interfaces/iuser.interface';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +10,7 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+	usersService = inject(UsersService)
+	arrUsers : IUser[] = []
 
 }

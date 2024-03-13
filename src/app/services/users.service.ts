1fe4481b56230107b0779db72cbe8e0dab0b1fe4
jgamonal <josue.gamonal@gmail.com>
2024-03-13 19:23:38 +0100

    Petición http creada en servicio

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { IUsers } from '../interfaces/iusers.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+	httpClient = inject(HttpClient)
+	baseUrl ='https://peticiones.online/api/users'
 
-  constructor() { }
+	getAllPromises() : Promise<IUsers>{
+		return lastValueFrom(this.httpClient.get<IUsers>(this.baseUrl))
+
+	}
 }

8c27307932ce703fc3ec4102bd150274024107ba
jgamonal <josue.gamonal@gmail.com>
2024-03-13 19:53:56 +0100

    conflicto resuelt0

--- src/app/interfaces/iuser.interface.ts
+++ src/app/interfaces/iuser.interface.ts
@@ -1,5 +1,4 @@
 export interface IUser {
-<<<<<<< HEAD
 
 		_id?: 		string;
 		id:         number;
@@ -8,14 +7,4 @@ export interface IUser {
 		username:   string;
 		email:      string;
 		image:      string;
-
-=======
-	_id?: 		string;
-	id:         number;
-    first_name: string;
-    last_name:  string;
-    username:   string;
-    email:      string;
-    image:      string;
->>>>>>> 1fe4481b56230107b0779db72cbe8e0dab0b1fe4
-}
+	}
